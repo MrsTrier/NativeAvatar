b095ef1f9470b57650f1a9fb9487f39f759427a5
Cheremina Daria <d.cheremina@gmail.com>
2024-11-22 08:20:51 +0100

    Initial Commit

---
+++ NativeAvatar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NativeAvatar
+//
+//  Created by Daria Cheremina on 22/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
